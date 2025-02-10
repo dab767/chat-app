c9998a9318857b72fdc42a2508db6d39f72e86be
dab767 <programmieren@lars-kailuweit.de>
2025-02-10 21:36:35 +0100

    delete logout

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -5,8 +5,7 @@ export const nodes = [
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/4')
 ];
 
 export const server_loads = [];
@@ -14,8 +13,7 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/login": [3],
-		"/logout": [~4],
-		"/register": [5]
+		"/register": [4]
 	};
 
 export const hooks = {
